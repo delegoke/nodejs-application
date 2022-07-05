46c4299edc8b375c376f7255cf1150aa91cd239e
delegoke <105636223+delegoke@users.noreply.github.com>
2022-07-05 10:47:32 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://35.183.145.19:9000/',
+  serverUrl: 'http://52.60.204.127:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': 'a7ec6830a8a86dbff54257fc7d5dee6f35e460e2',
+	    'sonar.login': 'fdfd2003b005779b3f31015517bfb166b2c40b72',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
